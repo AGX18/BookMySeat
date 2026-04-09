df94a6f0ac40f86e66a635414b20524be027c316
AGX <agxnh2020@gmail.com>
2026-04-09 23:01:35 +0200

    added the load balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,3 +83,70 @@ resource "aws_route_table_association" "a2" {
   subnet_id      = aws_subnet.subnet2.id
   route_table_id = aws_route_table.rt.id
 }
+
+resource "aws_security_group" "alb_sg" {
+  name   = "bookmyseat-alb-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bookmyseat-alb-sg"
+  }
+}
+
+resource "aws_lb" "alb" {
+  name               = "bookmyseat-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [
+    aws_subnet.subnet1.id,
+    aws_subnet.subnet2.id
+  ]
+  security_groups    = [aws_security_group.alb_sg.id]
+
+  tags = {
+    Name = "bookmyseat-alb"
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "bookmyseat-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  target_type = "ip"
+
+  health_check {
+    path = "/actuator/health"
+    port = "8080"
+  }
+
+  tags = {
+    Name = "bookmyseat-tg"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
