2436b8dd9981f44f75f36d5dbd20f2ab6ffc1ca3
AGX <agxnh2020@gmail.com>
2026-04-10 01:10:58 +0200

    added actuator

--- src/main/java/io/agx/bookmyseat/config/SecurityConfig.java
+++ src/main/java/io/agx/bookmyseat/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
                         // Swagger UI
                         .requestMatchers(
                                 "/swagger-ui/**",
